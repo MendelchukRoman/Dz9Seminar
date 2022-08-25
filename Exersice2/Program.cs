34fda0f13dd600a98a203807bae684530f79871c
kadzuki <heaven.roma@mail.ru>
2022-08-25 22:38:26 +0300

    Задача2

---
+++ Exersice2/Program.cs
@@ -0,0 +1,30 @@
+﻿// 
+int M = InputNumbers("Введите M: ");
+int N = InputNumbers("Введите N: ");
+int temp = M;
+
+if (M > N) 
+{
+  M = N; 
+  N = temp;
+}
+
+PrintSumm(M, N, temp=0);
+
+void PrintSumm(int M, int N, int sum)
+{
+  sum = sum + N;
+  if (N <= M)
+  {
+    Console.Write($"Сумма элементов= {sum} ");
+    return;
+  }
+  PrintSumm(M, N - 1, sum);
+}
+
+int InputNumbers(string input) 
+{
+  Console.Write(input);
+  int output = Convert.ToInt32(Console.ReadLine());
+  return output;
+}
