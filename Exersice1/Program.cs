7cfe124dc0df011244761bcd89c310ff150f9f90
kadzuki <heaven.roma@mail.ru>
2022-08-25 22:31:37 +0300

    Задача1

---
+++ Exersice1/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N
+
+int M = InputNumbers("Введите M: ");
+int N = InputNumbers("Введите N: ");
+
+PrintNumber(M, N);
+
+void PrintNumber(int M, int N)
+{
+Console.WriteLine($"N={N}, M={M}");
+if (M>N)
+for (int i = N; i <= M; i++)
+    Console.Write($" {i}");
+else
+    for (int i = M; i <= N; i++)
+        Console.Write($" {i}");
+}
+
+int InputNumbers(string input) 
+{
+  Console.Write(input);
+  int output = Convert.ToInt32(Console.ReadLine());
+  return output;
+}
